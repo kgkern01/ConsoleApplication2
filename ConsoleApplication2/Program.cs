1b4bf7b5597fe311829bced9268e3aef386d2184
kgkern01 <kgkern01@yahoo.com>
2017-01-25 20:35:34 -0500

    Parsing Lines

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApplication2
 {
@@ -7,7 +8,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            var fileName = @"C:\Projects\Test\Sample.txt";
+            var fileName = @"C:\Projects\Test\test.txt";
 
             // Read a text file using StreamReader 
             using (var sr = new System.IO.StreamReader(fileName))
@@ -16,12 +17,13 @@ namespace ConsoleApplication2
                 string pathName = "";
                 var outputParameterList = new List<string>();
                 var inputParameterList = new List<string>();
+                var programList = new List<Program>();
                 var newPath = true;
 
                 while ((line = sr.ReadLine()) != null)
                 {
-                    if (line.StartsWith("["))
-                    {
+                    //if (line.StartsWith("["))
+                    //{
                         if (outputParameterList.Count > 0 && newPath == false)
                         {
                             Console.WriteLine("Output parameters:");
@@ -42,19 +44,59 @@ namespace ConsoleApplication2
                         }
                         newPath = true;
                         pathName = line;
-                        var type = line.Substring(line.IndexOf("[") + 1, line.IndexOf("]"));
+                        var type = line.Substring(line.IndexOf("[") + 1, line.IndexOf("]")-1);
 
-                        var program = new Models.Program();
+                        var property = "";
+                        if(line.Contains(":"))
+                            property = line.Substring(line.IndexOf(":"));
+
+                        var decimalCount = line.Count(d => d == '.');
+
+                        var programName = "";
+                        var taskName = "";
+                        var toolName = "";
+                        var propertyName = "";
+
+                        var tool = line.Split('.');
+                        //programName = tool[0];
+                        //taskName = tool[1];
+                        //toolName = tool[2];
+
+
+                        switch (decimalCount)
                         {
+                            case 0:
+                                programName = tool[0].Substring(line.IndexOf("]") - 1);
+                                break;
+                            case 1:
+                                programName = tool[0];
+                                taskName = tool[1];
+                                break;
+                            case 2:
+                                programName = tool[0];
+                                taskName = tool[1];
+                                toolName = tool[2];
+                                break;
+                        }   
 
-                        };
+
+                        //var program = new Models.Program
+                        //{
+                        //    Name = line.Substring()
+                        //};
+
                         Console.WriteLine("Path: " + pathName);
+                        Console.WriteLine("Program: " + programName);
                         Console.WriteLine("Type:" + type);
-                    }
-                    else
-                    {
-                        newPath = false;
-                    }
+                        Console.WriteLine("Task: " + taskName);
+                        Console.WriteLine("Tool: " + toolName);
+                        Console.WriteLine("Property: " + property);
+                        Console.WriteLine("****************");
+                    //}
+                    //else
+                    //{
+                    //    newPath = false;
+                    //}
 
                     if (line.Contains("--->"))
                     {
@@ -74,9 +116,11 @@ namespace ConsoleApplication2
             Console.ReadLine();
         }
 
-        private static void ParseLine(string line)
+        private static void ParseLine(string line, int decimalCount)
         {
-            
+            var cells = line.Split('.');
+
+            var property = line.Substring(line.IndexOf(":") + 1, line.Length);
         }
     }
 }
